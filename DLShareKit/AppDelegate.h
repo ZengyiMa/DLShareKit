7f29e16dcafaaeac862beea94ab35b648cb0ab4b
famulei <mazengyi@famulei.com>
2016-08-29 19:18:42 +0800

    Initial Commit

---
+++ DLShareKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DLShareKit
+//
+//  Created by famulei on 8/29/16.
+//  Copyright © 2016 famulei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DLShareKit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DLShareKit
+//
+//  Created by famulei on 8/29/16.
+//  Copyright © 2016 famulei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
